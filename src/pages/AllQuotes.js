802cd40b771584ba8d359a40b07e674da390f2c7
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-09-02 16:25:49 +0530

    Worked on all-quotes & detai-quotes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,7 +1,7 @@
 import React from "react";
 import QuotesList from "../components/quotes/QuoteList";
 
-const DUMMY_QUOTES = [
+export const DUMMY_QUOTES = [
     { id: "q1", author: "Max", text: "Learning React is Fun!!" },
     { id: "q2", author: "Suraj", text: "Learning Python is Fun!!" },
     { id: "q3", author: "MaxMillion", text: "Learning Typescript is Fun!!" },
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import { DUMMY_QUOTES } from "./AllQuotes";
+import HighlightedQuote from "../components/quotes/HighlightedQuote"
 
 const QuoteDetail = () => {
     const params = useParams();
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+    if (!quote) {
+        return <p>No Quote found!</p>
+    }
     return (
         <>
-            <div>Quote Detail Page</div>
-            <p>{params.quoteId}</p>
-            <Route exact path={`/quotes/${params.quoteId}/comments`}>
+            <HighlightedQuote text={quote.text} author={quote.author} />
+            <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
         </>
