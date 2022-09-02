3097ffd507fec84ed8a8b39b659624018bf7f3f6
suraj-torinit <suraj.mahamuni@torinit.ca>
2022-09-02 19:07:24 +0530

    worked on rending comments

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import { DUMMY_QUOTES } from "./AllQuotes";
-import HighlightedQuote from "../components/quotes/HighlightedQuote"
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const QuoteDetail = () => {
     const params = useParams();
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+    const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
     if (!quote) {
-        return <p>No Quote found!</p>
+        return <p>No Quote found!</p>;
     }
     return (
         <>
             <HighlightedQuote text={quote.text} author={quote.author} />
+            <Route path={`/quotes/${params.quoteId}`} exact>
+                <div className="centered">
+                    <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+                        Load Comments
+                    </Link>
+                </div>
+            </Route>
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
